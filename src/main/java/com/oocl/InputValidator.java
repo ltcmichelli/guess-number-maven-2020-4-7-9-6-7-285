6d5cc7f58e7f18422262be2f3c7d318d1da5a32b
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-08 21:01:12 +0800

    [Michelle] InputValidator - change to List<Integer>

--- src/main/java/com/oocl/InputValidator.java
+++ src/main/java/com/oocl/InputValidator.java
@@ -5,17 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.lang.Integer.parseInt;
+
 public class InputValidator {
 
     public static final String ERROR_MSG = "Wrong Input, Input again";
 
     public String validate(String input) {
-        List<String> inputList = new ArrayList<String>(Arrays.asList(input.split(",")));
-        List<String> removeDuplicateNumList = inputList.stream().distinct().collect(Collectors.toList());
+        List<String> inputList = new ArrayList<>(Arrays.asList(input.split(",")));
+        List<Integer> removeDuplicateNumList = inputList.stream().map(Integer::parseInt).distinct().collect(Collectors.toList());
         if(removeDuplicateNumList.size() != 4){
             return ERROR_MSG;
         }
-
         return "";
     }
 }

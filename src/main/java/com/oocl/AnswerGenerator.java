32a943350c128f13cd7fec0cc781169caf7c9b52
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-08 19:09:12 +0800

    [Michelle] AnswerGenerator - should_generate_4_digit_number_when_generate_ans

--- src/main/java/com/oocl/AnswerGenerator.java
+++ src/main/java/com/oocl/AnswerGenerator.java
@@ -1,10 +1,27 @@
 package com.oocl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AnswerGenerator {
+    List<Integer> generatedNumList = new ArrayList<Integer>();
+
     public int generateNumber() {
         Random random = new Random();
         return random.nextInt(10);
+    }
+
+    public List<Integer> generateAnswer() {
+        int generateNum = generateNumber();
+        generatedNumList.add(generateNum);
+        do{
+            generateNum = generateNumber();
+            if (!generatedNumList.contains(generateNum)){
+                generatedNumList.add(generateNum);
+            }
+        }while (generatedNumList.size() < 4);
+
+        return generatedNumList;
     }
 }
--- src/test/java/com/oocl/AnswerGeneratorTest.java
+++ src/test/java/com/oocl/AnswerGeneratorTest.java
@@ -3,6 +3,8 @@ package com.oocl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class AnswerGeneratorTest {
     @Test
     public void should_1_digit_num_when_generateNumber(){
@@ -14,5 +16,17 @@ public class AnswerGeneratorTest {
 
         //then
         Assert.assertEquals(1, String.valueOf(result).length());
+    }
+
+    @Test
+    public void should_generate_4_digit_number_when_generate_ans(){
+        //given
+        AnswerGenerator generator = new AnswerGenerator();
+
+        //when
+        List<Integer> resultList = generator.generateAnswer();
+
+        //then
+        Assert.assertEquals(4, resultList.size());
     }
 }

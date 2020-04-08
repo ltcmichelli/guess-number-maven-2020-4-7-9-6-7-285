338965342a34bf1cf4a4168433ef4c75e45b40eb
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-08 22:52:51 +0800

    [Michelle] InputValidator - change to return boolean

--- src/main/java/com/oocl/InputValidator.java
+++ src/main/java/com/oocl/InputValidator.java
@@ -7,15 +7,13 @@ import java.util.stream.Collectors;
 
 public class InputValidator {
 
-    public static final String ERROR_MSG = "Wrong Input, Input again";
-
-    public String validate(String input) {
+    public boolean isValid(String input) {
         List<String> inputList = new ArrayList<>(Arrays.asList(input.split(",")));
         List<Integer> removeDuplicateNumList = inputList.stream().map(Integer::parseInt).distinct().collect(Collectors.toList());
-        if(removeDuplicateNumList.size() != 4){
-            return ERROR_MSG;
+        if (removeDuplicateNumList.size() != 4) {
+            return false;
         }
-        return "";
+        return true;
     }
 }
 
--- src/test/java/com/oocl/InputValidatorTest.java
+++ src/test/java/com/oocl/InputValidatorTest.java
@@ -5,41 +5,33 @@ import org.junit.Test;
 
 public class InputValidatorTest {
 
-    public static final String ERROR_MSG = "Wrong Input, Input again";
-
     @Test
-    public void should_no_msg_given_valid_input(){
+    public void should_return_true_given_valid_input() {
         //given
         InputValidator validator = new InputValidator();
         String input = "1,2,3,4";
-        //when
-        String result = validator.validate(input);
 
         //then
-        Assert.assertEquals("", result);
+        Assert.assertTrue(validator.isValid(input));
     }
 
     @Test
-    public void should_error_msg_given_input_is_less_than_4_digit(){
+    public void should_return_false_given_input_is_less_than_4_digit() {
         //given
         InputValidator validator = new InputValidator();
         String input = "1,2,3";
-        //when
-        String result = validator.validate(input);
 
         //then
-        Assert.assertEquals(ERROR_MSG, result);
+        Assert.assertFalse(validator.isValid(input));
     }
 
     @Test
-    public void should_error_msg_given_input_digit_is_repeated(){
+    public void should_return_false_given_input_digit_is_repeated() {
         //given
         InputValidator validator = new InputValidator();
         String input = "1,1,3,5";
-        //when
-        String result = validator.validate(input);
 
         //then
-        Assert.assertEquals(ERROR_MSG, result);
+        Assert.assertFalse(validator.isValid(input));
     }
 }

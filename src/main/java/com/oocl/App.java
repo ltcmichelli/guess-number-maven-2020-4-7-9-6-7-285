2f095c1d2285476a5a0eb28781e49c1057226529
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-14 19:09:43 +0800

    [Michelle] Remove unused import

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,11 +1,5 @@
 package com.oocl;
 
-import java.io.IOException;
-import java.util.Scanner;
-
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         GuessNumberGame game = new GuessNumberGame();
